6f2a3c208c95dd42adddb70558ae04f5ae4df011
Paul Swartz <paul@paulswartz.net>
2023-09-05 10:44:02 -0400

    feat: build image and push to ghcr.io

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+set -e
+image="$1"
+tag="$2"
 
-docker rmi symptoma/activemq
-docker build -t symptoma/activemq .
+if [ -z "$FORCE_BUILD" ] && docker manifest inspect "$image:$tag" > /dev/null 2>&1; then
+    echo "Skipping $image:$tag, already exists."
+    exit 0
+fi
+
+docker build --build-arg "ACTIVEMQ_VERSION=$tag" \
+    --tag "$image:$tag" .
+docker push "$image:$tag"
